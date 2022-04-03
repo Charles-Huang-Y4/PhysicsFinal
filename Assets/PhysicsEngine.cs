5261e56a6108a226bad5f0e3e781a2f2f9c35b70
acm <alexis_cm@live.com>
2022-04-02 20:38:01 -0700

    Attempted UI integration with phyhsicsEngine

--- Assets/PhysicsEngine.cs
+++ Assets/PhysicsEngine.cs
@@ -1,53 +1,61 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhysicsEngine : MonoBehaviour {
-    [SerializeField] private GameObject _sphere;
-    [SerializeField] private float _initialAngularVelocity;
-    [SerializeField] private float _mass;
+    [SerializeField] private UIManager _uiMgr;
+    [SerializeField] private GameObject _physicsObj;
+    [SerializeField] private Slider _massSlider;
+    [SerializeField] private Slider _radiusSlider;
+    [SerializeField] private float _initialOmega;
 
     private float _r2d2;
     private float _nextAngle;
     private float _radius;
     private float _height;
+    private float _mass;
     private float _angularMomentum;
     private float _angularVelocity;
 
     void Start() {
         _r2d2 = 180 / Mathf.PI;
         _nextAngle = 0;
-        _radius = _sphere.transform.localScale.x / 2;
-        _height = _sphere.transform.localScale.y;
-        _angularMomentum = GetAngMo(_initialAngularVelocity);
-        _angularVelocity = _initialAngularVelocity;
+        _radiusSlider.value = _physicsObj.transform.localScale.x / 2;
+        _height = _physicsObj.transform.localScale.y;
+        _radius = _radiusSlider.value;
+        _angularMomentum = GetAngMo(_initialOmega);
+        _angularVelocity = _initialOmega;
+        _mass = _massSlider.value;
     }
 
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.W)) {
-            IncrementRadius(0.1f);
-            SetNewVel();
-            print(_angularVelocity);
-        } else if (Input.GetKeyDown(KeyCode.S)) {
-            IncrementRadius(-0.1f);
-            SetNewVel();
-            print(_angularVelocity);
-        }
-    }
+    //private void Update() {
+    //    if (Input.GetKeyDown(KeyCode.W)) {
+    //        SetRadius(0.1f);
+    //        CalculateAngularVelocity();
+    //        print(_angularVelocity);
+    //    } else if (Input.GetKeyDown(KeyCode.S)) {
+    //        SetRadius(-0.1f);
+    //        CalculateAngularVelocity();
+    //        print(_angularVelocity);
+    //    }
+    //}
 
     void FixedUpdate() {
         _nextAngle += _angularVelocity * _r2d2 * Time.fixedDeltaTime;
-        _sphere.transform.rotation = Quaternion.AngleAxis(_nextAngle, Vector3.up);
+        _physicsObj.transform.rotation = Quaternion.AngleAxis(_nextAngle, Vector3.up);
     }
 
-    public void IncrementRadius(float num) {
-        float newRadius = _radius + num;
-
-        if (newRadius <= 0.1) {
-            return;
-        }
-
-        _radius = newRadius;
+    public void SetRadius() {
+        _radius = _radiusSlider.value;
         Vector3 newScale = new Vector3(_radius * 2, _height, _radius * 2);
-        _sphere.transform.localScale = newScale;
+        _physicsObj.transform.localScale = newScale;
+        _uiMgr.UpdateRadius(_radius.ToString());
+        CalculateAngularVelocity();
+    }
+
+    public void SetMass() {
+        _mass = _massSlider.value;
+        _uiMgr.UpdateMass(_mass.ToString());
+        CalculateAngularVelocity();
     }
 
     /// <summary>
@@ -59,9 +67,9 @@ public class PhysicsEngine : MonoBehaviour {
     }
 
     /// <summary>
-    /// sets new velocity 
+    /// sets new angular velocity 
     /// </summary>
-    private void SetNewVel() {
+    private void CalculateAngularVelocity() {
         _angularVelocity = _angularMomentum / (_mass *_radius);
     }
 }
